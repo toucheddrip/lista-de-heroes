babf44011201ec25ca4e57e53dc6156ff3da4ed1
Jose luis Michel <jose.hernandez4299@alumnos.udg.mx>
2022-10-08 14:07:03 -0500

    componente heroes

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
